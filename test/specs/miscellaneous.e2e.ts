22996c99afaa5a93b7aa5b7d6618039b1112a993
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-10-22 23:51:36 -0300

    chore: remove xstep import

--- test/specs/miscellaneous.e2e.ts
+++ test/specs/miscellaneous.e2e.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { step, xstep } from 'mocha-steps';
+import { step } from 'mocha-steps';
 import { TestSetup } from '../testSetup';
 import * as utilities from '../utilities/index';
 import { expect } from 'chai';
