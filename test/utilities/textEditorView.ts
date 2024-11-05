0d6b97ec78f3af5e860981ec9048a92111e3a58d
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-11-05 20:43:31 -0300

    feat: metadataDeployRetrieve test in redhat

--- test/utilities/textEditorView.ts
+++ test/utilities/textEditorView.ts
@@ -1,6 +1,7 @@
-import { TextEditor, Workbench } from 'vscode-extension-tester';
+import { EditorView, TextEditor, Workbench } from 'vscode-extension-tester';
 import { executeQuickPick } from './commandPrompt';
-import { Duration, log, pause } from './miscellaneous';
+import { Duration, log, openFile, pause } from './miscellaneous';
+import { getBrowser } from './workbench';
 
 /**
  * @param workbench page object representing the custom VSCode title bar
@@ -16,4 +17,35 @@ export async function getTextEditor(workbench: Workbench, fileName: string): Pro
   const editorView = workbench.getEditorView();
   const textEditor = (await editorView.openEditor(fileName)) as TextEditor;
   return textEditor;
+}
+
+export async function checkFileOpen(
+  workbench: Workbench,
+  name: string,
+  options: { msg?: string; timeout?: Duration } = { timeout: Duration.milliseconds(10_000) }
+) {
+  await getBrowser().wait(
+    async () => {
+      try {
+        const editorView = workbench.getEditorView();
+        const activeTab = await editorView.getActiveTab();
+        if (activeTab != undefined && name == (await activeTab.getTitle())) {
+          return true;
+        } else return false;
+      } catch (error) {
+        return false;
+      }
+    },
+    options.timeout?.milliseconds,
+    options.msg ?? `Expected to find file ${name} open in TextEditor before ${options.timeout}`,
+    500 // Check every 500 ms
+  );
+}
+
+export async function attemptToFindTextEditorText(filePath: string): Promise<string> {
+  await openFile(filePath);
+  const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+  const editorView = new EditorView();
+  const editor = await editorView.openEditor(fileName);
+  return await editor.getText();
 }
