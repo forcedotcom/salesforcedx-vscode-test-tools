cd66963bd11b970f8db23d6365f939abb97cbea5
Daphne Yang <daphne.yang@salesforce.com>
2025-07-25 13:34:34 -0400

    chore: fix typo

--- src/salesforce-components/deployAndRetrieve.ts
+++ src/salesforce-components/deployAndRetrieve.ts
@@ -124,7 +124,7 @@ const constructExpectedTexts = (
 
   const metadataPaths: Record<string, string> = {
     ApexClass: `force-app${pathSeparator}main${pathSeparator}default${pathSeparator}classes`,
-    ExternalServiceRsegistration: `force-app${pathSeparator}main${pathSeparator}default${pathSeparator}externalServiceRegistrations`,
+    ExternalServiceRegistration: `force-app${pathSeparator}main${pathSeparator}default${pathSeparator}externalServiceRegistrations`,
     CustomObject: `force-app${pathSeparator}main${pathSeparator}default${pathSeparator}objects`,
     CustomField: `force-app${pathSeparator}main${pathSeparator}default${pathSeparator}objects`
   };
